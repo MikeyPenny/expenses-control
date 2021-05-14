55d112109acb237f92b385e56fde6b2567fd4d1d
Miguel Sandoval <miguelship3@gmail.com>
2021-05-14 14:39:30 +0200

    Added ItemList for each item component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import ExpenseList from './components/ExpenseList';
 import './App.css';
 
 function App() {
+
+  const expenses = [
+    {id: 'e1', title: 'Toilet paper', date: new Date(2020, 7, 14), amount: 94.12},
+    {id: 'e2', title: 'New TV', date: new Date(2021, 2, 12), amount: 799.49},
+    {id: 'e3', title: 'Car insurance', date: new Date(2021, 2, 28), amount: 294.67},
+    {id: 'e4', title: 'New Desk', date: new Date(2021, 5, 12), amount: 450}
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started</h2>
+      <ExpenseList expenses={expenses} />
     </div>
   );
 }
